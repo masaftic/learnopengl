0fe349f3345eced03690045f9031908ec39f2e2f
masaftic <yousefalaa618y@gmail.com>
2024-04-21 17:11:01 +0200

    a game class

---
+++ include/game.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+enum GameState
+{
+	GAME_ACTIVE,
+	GAME_MENU,
+	GAME_WIN
+};
+
+class Game
+{
+public:
+	GameState State;
+	bool Keys[1024];
+	unsigned int Width, Height;
+
+	Game(unsigned int width, unsigned int height);
+	~Game();
+
+	void Init();
+	void ProcessInput(float dt);
+	void Update(float dt);
+	void Render();
+};
+
---
+++ src/game.cpp
@@ -0,0 +1,25 @@
+#include "game.h"
+
+Game::Game(unsigned int width, unsigned int height)
+{
+}
+
+Game::~Game()
+{
+}
+
+void Game::Init()
+{
+}
+
+void Game::ProcessInput(float dt)
+{
+}
+
+void Game::Update(float dt)
+{
+}
+
+void Game::Render()
+{
+}
