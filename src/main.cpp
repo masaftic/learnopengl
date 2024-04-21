be5e19f11675632733e62134a3ec88d53b7b297b
masaftic <yousefalaa618y@gmail.com>
2024-04-21 15:55:53 +0200

    folder structure

--- src/main.cpp
+++ src/main.cpp
@@ -106,9 +106,9 @@ int main()
     // Specify the viewport of OpenGL in the Window
     // In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
     glViewport(0, 0, width, height);
-
-
-    Shader shaderProgram("default_Vert.c", "default_Frag.c");
+     
+    // they are .c files to trick visual studio so i can have syntax highlighting
+    Shader shaderProgram("resources/shaders/default_Vert.c", "resources/shaders/default_Frag.c");
     
 
     VAO VAO1;
@@ -124,11 +124,11 @@ int main()
     VBO1.Unbind();
     // EBO1.Unbind();
 
-    Texture texture("minato.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+    Texture texture("resources/textures/minato.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 
     texture.texUniform(shaderProgram, "tex0", 0); 
 
-    Texture texture2("chad.png", GL_TEXTURE_2D, GL_TEXTURE1, GL_RGBA, GL_UNSIGNED_BYTE);
+    Texture texture2("resources/textures/chad.png", GL_TEXTURE_2D, GL_TEXTURE1, GL_RGBA, GL_UNSIGNED_BYTE);
 
     texture2.texUniform(shaderProgram, "tex1", 1);
 
